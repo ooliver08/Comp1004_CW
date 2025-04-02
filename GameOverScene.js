48a93744db7dfb67eeab844252b215dd8d0299a3
ooliver08 <oscar.oliver@students.plymouth.ac.uk>
2025-04-02 16:20:52 +0100

    adjusting game over message

--- GameOverScene.js
+++ GameOverScene.js
@@ -7,10 +7,10 @@ class GameOverScene extends Phaser.Scene {
         //Add background
         this.add.rectangle(0, 0, this.scale.width, this.scale.height, 0x000000, 0.5).setOrigin(0);
         //Display Game Over
-        this.add.bitmapText(this.scale.width / 2, this.scale.height / 2 - 50, "pixelFont", "GAME OVER", 32).setOrigin(0.5);
+        this.add.bitmapText(this.scale.width / 2, this.scale.height / 2 - 50, "pixelFont", "-GAME OVER-", 32).setOrigin(0.5);
         //Display final score
         const score = this.registry.get("score") || 0;  //Get final score
-        this.add.bitmapText(this.scale.width / 2, this.scale.height / 2, "pixelFont", `SCORE: ${score}`, 24).setOrigin(0.5);
+        this.add.bitmapText(this.scale.width / 2, this.scale.height / 2, "pixelFont", `FINAL SCORE: ${score}`, 24).setOrigin(0.5);
         let returnButton = this.add.bitmapText(this.scale.width / 2, this.scale.height / 2 + 50, "pixelFont", "Return to Menu", 16)
 			.setOrigin(0.5)
 			.setInteractive();
